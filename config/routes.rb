c5ee202d36bba850115195372464fb95dab2703e
Famitiki0420 <disillusion0420@gmail.com>
2021-05-25 19:59:44 +0000

    devise初期設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,45 @@
 Rails.application.routes.draw do
-  devise_for :installs
+  devise_for :admin, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+  }
+  namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
+    # get 'search' => 'homes#search', as: 'search'
+    # get 'customers/:customer_id/orders' => 'orders#index', as: 'customer_orders'
+    # resources :customers, only: [:index, :show, :edit, :update]
+    # resources :items, except: [:destroy]
+    # resources :genres, only: [:index, :create, :edit, :update]
+    # resources :orders, only: [:index, :show, :update] do
+    #   resources :order_details, only: [:update]
+    # end
+  end
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+  }
+  
+    scope module: :users do
+    root 'homes#top'
+    # get 'customers/mypage' => 'customers#show', as: 'mypage'
+    # get 'customers/information/edit' => 'customers#edit', as: 'edit_information'
+    # patch 'customers/information' => 'customers#update', as: 'update_information'
+    # put 'customers/information' => 'customers#update'
+    # get 'customers/unsubscribe' => 'customers#unsubscribe', as: 'confirm_unsubscribe'
+    # patch 'customers/withdraw' => 'customers#withdraw', as: 'withdraw_customer'
+    # put 'customers/withdraw' => 'customers#withdraw'
+    # delete 'cart_items/destroy_all' => 'cart_items#destroy_all', as: 'destroy_all_cart_items'
+    # post 'orders/confirm' => 'orders#confirm'
+    # get 'orders/confirm' => 'orders#error'
+    # get 'orders/thanks' => 'orders#thanks', as: 'thanks'
+
+    # resources :addresses, only: [:index, :create, :edit, :update, :destroy]
+    # resources :items, only: [:index, :show] do
+    #   resources :cart_items, only: [:create, :update, :destroy]
+    # end
+    # resources :cart_items, only: [:index]
+    # resources :orders, only: [:new, :index, :create, :show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
