ad51a5bd34e9c74d1a2a7c17900a48bf987ce380
Famitiki0420 <disillusion0420@gmail.com>
2021-05-29 07:41:19 +0000

    fixed db schema

--- db/migrate/20210520161605_devise_create_users.rb
+++ db/migrate/20210520161605_devise_create_users.rb
@@ -34,7 +34,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
       t.string :name, null: false, default: "ニックネーム"
       t.string :image_id, null: false
-      t.text :introduction, null: false, default: ""
+      t.text :introduction, null: false
       t.timestamps null: false
     end
 
